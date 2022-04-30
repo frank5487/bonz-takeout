500cbf0803d4427190ba0cf5918f299e5e60fa04
Yung-Jen <frank5d2008@gmail.com>
2022-04-30 13:24:16 -0400

    map static resources

---
+++ src/main/java/en/upenn/bonz/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package en.upenn.bonz.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Slf4j
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+    /**
+     * map static resources
+     * @param registry
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+        log.info("start to map static resources");
+    }
+}
