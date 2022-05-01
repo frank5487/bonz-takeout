4538a98077d40bd03a9e630ae7cac2c1f62def66
Yung-Jen <frank5d2008@gmail.com>
2022-05-01 10:24:33 -0400

    extends message convert to mvc

--- src/main/java/en/upenn/bonz/config/WebMvcConfig.java
+++ src/main/java/en/upenn/bonz/config/WebMvcConfig.java
@@ -1,9 +1,14 @@
 package en.upenn.bonz.config;
 
+import en.upenn.bonz.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -18,5 +23,20 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
         log.info("start to map static resources");
+    }
+
+    /**
+     * extends message converters from mvc framework
+     * @param converters
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // create message converter object
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        // set message convert configuration
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+        // add to mvc framework, 0 rerpresents that it this self-define message converter would be used in the first priority
+        converters.add(0, messageConverter);
+
     }
 }
