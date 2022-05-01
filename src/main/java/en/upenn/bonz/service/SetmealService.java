a784bc9df93c5db48e7acf3da51187c034eefc70
Yung-Jen <frank5d2008@gmail.com>
2022-05-01 13:32:22 -0400

    set up category dish setmeal class mvc

--- src/main/java/en/upenn/bonz/service/SetmealService.java
+++ src/main/java/en/upenn/bonz/service/SetmealService.java
@@ -2,7 +2,6 @@ package en.upenn.bonz.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import en.upenn.bonz.entity.Setmeal;
-import org.apache.ibatis.annotations.Mapper;
 
 public interface SetmealService extends IService<Setmeal> {
 }
