51718c6d37e7d13849cdaa0b34da4167d085cee7
Yung-Jen <frank5d2008@gmail.com>
2022-04-30 23:46:17 -0400

    Pagination plugin set up

---
+++ src/main/java/en/upenn/bonz/config/MybatisPlusConfig.java
@@ -0,0 +1,21 @@
+package en.upenn.bonz.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * set configuration of pagination for MybatisPlus
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+
+        return mybatisPlusInterceptor;
+    }
+}
