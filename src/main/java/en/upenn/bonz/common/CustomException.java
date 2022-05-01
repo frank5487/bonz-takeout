02ad8213742c22309ff0fe2125d8838cfb28748e
Yung-Jen <frank5d2008@gmail.com>
2022-04-30 23:21:56 -0400

    handle Exception

---
+++ src/main/java/en/upenn/bonz/common/CustomException.java
@@ -0,0 +1,8 @@
+package en.upenn.bonz.common;
+
+public class CustomException extends RuntimeException{
+
+    public CustomException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/en/upenn/bonz/common/GlobalExceptionHandler.java
@@ -0,0 +1,46 @@
+package en.upenn.bonz.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@Slf4j
+@ControllerAdvice(annotations = {RestController.class, Controller.class})
+@ResponseBody
+public class GlobalExceptionHandler {
+
+    /**
+     * deal with exception cases
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public R<String>  exceptionHandler(SQLIntegrityConstraintViolationException ex) {
+        log.error(ex.getMessage());
+
+        if (ex.getMessage().contains("Duplicate entry")) {
+            String[] words = ex.getMessage().split(" ");
+            String msg = words[2] + "has existed...";
+            return R.error(msg);
+        }
+
+        return R.error("Unknown error");
+    }
+
+    /**
+     * deal with custom exception cases
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(CustomException.class)
+    public R<String> exceptionHandler(CustomException ex) {
+        log.error(ex.getMessage());
+
+        return R.error(ex.getMessage());
+    }
+}
