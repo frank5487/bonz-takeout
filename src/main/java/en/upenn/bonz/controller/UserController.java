8273b2c15ccec16d2a2c3cbea69645794d505751
Yung-Jen <frank5d2008@gmail.com>
2022-05-03 16:29:25 -0400

    store verified code in Redis

--- src/main/java/en/upenn/bonz/controller/UserController.java
+++ src/main/java/en/upenn/bonz/controller/UserController.java
@@ -7,6 +7,7 @@ import en.upenn.bonz.utils.ValidateCodeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @RestController
@@ -24,6 +26,15 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * send verified code
+     * @param user
+     * @param session
+     * @return
+     */
     @PostMapping("/sendMsg")
     public R<String> sendMsg(@RequestBody User user, HttpSession session) {
         // get phone #
@@ -34,13 +45,24 @@ public class UserController {
             String code = ValidateCodeUtils.generateValidateCode(4).toString();
             log.info(code);
 
-            session.setAttribute(phone, code);
+            // store code in session
+            //session.setAttribute(phone, code);
+
+            // store code in Redis, and set expiration up to 5 mins
+            redisTemplate.opsForValue().set(phone, code, 5, TimeUnit.MINUTES);
+
             return R.success("send SMS successfully...");
         }
 
         return R.error("send SMS failed...");
     }
 
+    /**
+     * user login
+     * @param map
+     * @param session
+     * @return
+     */
     @PostMapping("/login")
     public R<User> login(@RequestBody Map map, HttpSession session) {
         log.info(map.toString());
@@ -51,10 +73,14 @@ public class UserController {
         String code = map.get("code").toString();
 
         // get code from session
-        String codeInSession = (String) session.getAttribute(phone);
+        //String codeInSession = (String) session.getAttribute(phone);
 
-        // verify code
-        if (codeInSession != null && codeInSession.equals(code)) {
+        // get code from redis
+        String codeInRedis = (String) redisTemplate.opsForValue().get(phone);
+
+        // compare two verified code
+        //if (codeInSession != null && codeInSession.equals(code)) {
+        if (codeInRedis != null && codeInRedis.equals(code)) {
             User user = userService.getUser(phone);
 
             if (user == null) {
@@ -66,12 +92,20 @@ public class UserController {
 
             session.setAttribute("user", user.getId());
 
+            // if user login successfully, we can delete code in Redis
+            redisTemplate.delete(phone);
+
             return R.success(user);
         }
 
         return R.error("wrong code");
     }
 
+    /**
+     * user log out
+     * @param request
+     * @return
+     */
     @PostMapping("/logout")
     public R<String> logout(HttpServletRequest request) {
         request.getSession().removeAttribute("user");
